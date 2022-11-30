f705b3f7a76ea70368fb44fc59e5a2ea4d6cb39c
OrJDev <orjdeveloper@gmail.com>
2022-11-30 15:21:08 +0200

    use solidauth monorepo

--- src/routes/api/auth/[...solidauth].ts
+++ src/routes/api/auth/[...solidauth].ts
@@ -1,5 +1,5 @@
 import { authenticator } from "~/server/auth";
-import { createSolidAuthHandler } from "solid-auth";
+import { createSolidAuthHandler } from "@solid-auth/core";
 import { type User } from "@prisma/client";
 
 const handler = createSolidAuthHandler<User>(authenticator);
